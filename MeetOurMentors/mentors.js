4dcbe95f51aed9a8b6f63afa1eeb50c53cd6be15
latifatquadri <quadrilatifat03@gmail.com>
2025-11-18 12:55:19 +0100

    Updated mentors page

--- MeetOurMentors/mentors.js
+++ MeetOurMentors/mentors.js
@@ -28,3 +28,9 @@ const modal = document.getElementById("formModal");
 
     this.reset();
 });
+
+  fetch("../footer.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
